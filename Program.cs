2774d6b4e32be3622a520a1cf29966c40c91434a
Marcus Rocha <mvcsr2208@hotmail.com>
2025-08-05 21:25:47 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,38 +7,37 @@ namespace CalculadoraWeb
         static void Main(string[] args)
         {
             Console.WriteLine("Calculadora Colaborativa Web");
-            // Integrante 1: Implementar menu e chamar as funções
+            // Integrante 1: Implementar menu e chamar as funções 
         }
 
-        // Integrante 1
+        // Integrante 1 (João Victor)
         public static double Adicao(double a, double b)
         {
             return a + b;
         }
 
-        // Integrante 2
-        // public static double Subtracao(double a, double b)
-        // {
-        //     // Implementar aqui
-        // }
+        // Integrante 2 (Matheus)
+        public static double Subtracao(double a, double b)
+         {
+            return a - b;
+         }
 
-        // Integrante 3
-        // public static double Multiplicacao(double a, double b)
-        // {
-        //     // Implementar aqui
-        // }
+        // Integrante 3 (Sophia)
+        public static double Multiplicacao(double a, double b)
+        {
+        return a * b;
+        }
 
-        // Integrante 4
+        // Integrante 4 (Adam)
         public static double Divisao(double a, double b)
         {
             return a / b;
         }
         
-        // Integrante 5
+        // Integrante 5 (Marcus Rocha)
         public static double Potenciacao(double NumeroBase, double Expoente)
         {
             return Math.Pow(NumeroBase, Expoente)
         }
     }
 }
-
